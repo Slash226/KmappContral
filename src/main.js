542fa6351709c53b02025898197148b7796820e7
Suezp <769198688@qq.com>
2021-01-31 23:20:06 +0800

    添加Vuerouter

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
 
+vue.use(VueRouter)
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+    render: h => h(App)
 }).$mount('#app')
